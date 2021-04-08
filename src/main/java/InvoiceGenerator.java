7a0c4139fd32eb29ce2d4baeabbc27fb1abd4ef4
Shubham <sm.phoujdar@gmail.com>
2021-04-08 14:06:43 +0530

    Refactored: TC1 which passes. Returns correct fare for TC1.

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,6 +1,9 @@
 public class InvoiceGenerator {
 
+    public static final int COST_PER_KILOMETER = 10;
+    public static final int COST_PER_MINUTE = 1;
+
     public int calculateFare(Ride currentRide) {
-        return 0;
+        return currentRide.getDistance()*COST_PER_KILOMETER + currentRide.getTime()*COST_PER_MINUTE;
     }
 }
--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -1,6 +1,25 @@
 public class Ride {
-
+    private int distance;
+    private int time;
 
     public Ride(int distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
     }
 }
